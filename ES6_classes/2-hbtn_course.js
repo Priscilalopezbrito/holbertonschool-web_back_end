3cfeacf96d6a2c67a439aced5d22fa486a2ec2f6
priscilalopez <prisscilalb@gmail.com>
2024-11-11 21:44:38 -0400

    fix setters

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,13 +1,13 @@
 class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name === 'string') {
-        throw new TypeError('Students must be a string');
+      throw new TypeError('Students must be a string');
     }
     if (typeof length === 'number') {
-        throw new TypeError('Length must be a number');
+      throw new TypeError('Length must be a number');
     }
-    if (!Array.isArray(setStudents) || !setStudents.every((student) => typeof student === 'string')) {
-        throw new TypeError('Students must be a string');
+    if (!Array.isArray(students) || !students.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be a string');
     }
     this._name = name;
     this._length = length;
@@ -19,7 +19,9 @@ class HolbertonCourse {
   }
 
   set name(setName) {
-
+    if (typeof setName !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
     this._name = setName;
   }
 
@@ -28,7 +30,9 @@ class HolbertonCourse {
   }
 
   set length(setLength) {
-
+    if (typeof setLength !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
     this._length = setLength;
   }
 
@@ -37,7 +41,9 @@ class HolbertonCourse {
   }
 
   set students(setStudents) {
-
+    if (!Array.isArray(setStudents) || !setStudents.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
     this._students = setStudents;
   }
 }
