655993c5750352f22f82e2b46cf51c43592cb6de
priscilalopez <prisscilalb@gmail.com>
2024-11-18 18:05:44 -0400

    fix eslint file 4

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -4,9 +4,9 @@ export default function updateStudentGradeByCity(getListStudents, city, newGrade
   }
 
   return getListStudents
-    .filter(student => student.location === city)
-    .map(student => {
-      const gradeOj = newGrades.find(grade => grade.studentId === student.id);
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeOj = newGrades.find((grade) => grade.studentId === student.id);
       return {
         ...student,
         grade: gradeOj ? gradeOj.grade : 'N/A',
