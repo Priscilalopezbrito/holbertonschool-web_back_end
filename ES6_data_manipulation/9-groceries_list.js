80fa6fbde9fbdb07ea7f49d47ea913d0234787f6
priscilalopez <prisscilalb@gmail.com>
2024-11-19 11:35:42 -0400

    Adding file 9-groceries_list.js

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  const item = new Map();
+  item.set('Apples', 10);
+  item.set('Tomatoes', 10);
+  item.set('Pasta', 1);
+  item.set('Rice', 1);
+  item.set('Banana', 5);
+
+  return item;
+}
