7c5f7d5d67d266af07a0404c542dcbb0a9429d0f
priscilalopez <prisscilalb@gmail.com>
2024-11-20 11:35:49 -0400

    fix eslint

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,5 +1,4 @@
-/* eslint-disable */
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([
