466141730ac2332e08c6de029bdf9835ee26b570
priscilalopez <prisscilalb@gmail.com>
2024-11-20 10:42:19 -0400

    test handlers in file 2-then.js

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -2,17 +2,16 @@
 export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => {
-      console.log('Got a response from the API');
       return {
         status: 200,
         body: 'success',
       };
     })
     .catch(() => {
-      console.log('Got a response from the API');
-      return Error();
+      return new Error();
     })
     .finally(() => {
       console.log('Got a response from the API');
     });
+
 }
