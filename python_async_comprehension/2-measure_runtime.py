be869000438137bc28654e1b311ca032077c9b79
priscilalopez <prisscilalb@gmail.com>
2024-12-10 11:06:19 -0400

    Fix asyncio. gather format in file 2-measure_runtime.py

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -5,18 +5,13 @@ import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """
     Execute async_comprehension four times
     in parallel using asyncio.gather
     """
     start = time.perf_counter()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
+    await asyncio.gather(*(async_comprehension() for i in range(4)))
     end = time.perf_counter()
 
     return end - start
