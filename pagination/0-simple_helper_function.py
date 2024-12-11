801b1909dbaeac6c798fab36b468cd63ea814e02
priscilalopez <prisscilalb@gmail.com>
2024-12-11 16:50:56 -0400

    Adding documentation in file 0-simple_helper_function.py

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -4,6 +4,14 @@ from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Args:
+        page (int): Page number
+        page_size (int): Page size
+
+    Returns:
+        Tuple[int, int]: Page range
+    """
     start = (page - 1) * page_size
     end = start + page_size
     return start, end
