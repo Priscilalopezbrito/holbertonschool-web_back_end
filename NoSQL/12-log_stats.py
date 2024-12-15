a08aaac59c02807cfd1903123e0c12baa62a437d
priscilalopez <prisscilalb@gmail.com>
2024-12-15 19:10:54 -0400

    Adding file 12-log_stats.py

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" Log stats """
+from pymongo import MongoClient
+
+
+def stats():
+    """ Log stats """
+    client = MongoClient('localhost', 27017)
+    db = client.logs
+    collection = db.nginx
+
+    # Total number of logs
+    total_logs = collection.count_documents({})
+    print(f"{total_logs} logs")
+
+    # Methods
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    status_check = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check} status check")
+
+
+if __name__ == "__main__":
+    stats()
