6deedd769d2bae2e21181f6a66d7869085a9c2d8
priscilalopez <prisscilalb@gmail.com>
2025-01-26 16:34:54 -0400

    Adding file 7-http_express.js

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const path = process.argv[2];
+
+const app = express();
+app.get('/', (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('This is the list of our students\n');
+  try {
+    const studentsOutput = await countStudents(path);
+    res.end(studentsOutput);
+  } catch (error) {
+    res.end('Cannot load the database');
+  }
+});
+
+app.listen(1245);
+module.exports = app;
